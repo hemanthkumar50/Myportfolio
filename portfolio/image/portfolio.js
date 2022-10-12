c7366b9ff4ed16096ae9a9bc4f767e4466c84321
hemanthkumar50 <111850243+hemanthkumar50@users.noreply.github.com>
2022-10-12 10:01:22 +0530

    Add files via upload

---
+++ portfolio/image/portfolio.js
@@ -0,0 +1,8 @@
+// typing styling
+var typed = new Typed(".element", {
+    strings : ["Coder.","Designer.","Cricketer.","Youtuber."],
+    typespeed:1800,
+    backspeed:1600,
+    loop:true
+});
+
